98546438aa8bbc6b0ac1f465e054628f316fd328
Warren Hong <hwhong1129@gmail.com>
2023-07-11 22:36:01 -0700

    til states updater func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,105 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import styles from "./page.module.css";
+import React from "react";
+import classNames from "classnames";
+
+const N = 10;
+
+interface Coordionate {
+  x: number;
+  y: number;
+}
+
+const def = [
+  { x: 5, y: 5 },
+  // { x: 5, y: 4 },
+  //{ x: 5, y: 3 },
+];
+
+/**
+ * State behaves like a snapshot. When we call three setState consecutively
+ * ----------
+ * setCount(count + 1)
+ * setCount(count + 1)
+ * setCount(count + 1)
+ * ----------
+ * the count will ultimatly be just 1.
+ * Because a state variable's value never changes within a render.
+ * Need to use updater function to use the same state variable before the next render
+ */
 
 export default function Home() {
+  const [coords, setCoords] = React.useState<Coordionate[]>(def);
+
+  const requestRef = React.useRef<number>(0);
+  const previousTimeRef = React.useRef<number>();
+
+  // const animate = (time: any) => {
+  //   if (previousTimeRef.current != undefined) {
+  //     const deltaTime = time - previousTimeRef.current;
+
+  //     // const newCoords = [];
+  //     // for (let i = 0; i < coords.length; i++) {
+  //     //   newCoords.push({ x: coords[i].x, y: coords[i].y + 1 });
+  //     // }
+  //     setCoords((prevCoords) =>
+  //       prevCoords.map(({ x, y }) => ({ x, y: y + 1 }))
+  //     );
+
+  //     setCount((prevCount) => (prevCount + deltaTime * 0.01) % 100);
+  //   }
+
+  //   previousTimeRef.current = time;
+  //   requestRef.current = requestAnimationFrame(animate);
+  // };
+
+  React.useEffect(() => {
+    // requestRef.current = requestAnimationFrame(animate);
+    // return () => cancelAnimationFrame(requestRef.current);
+    function update(currentDelta: number) {
+      requestRef.current = requestAnimationFrame(update);
+
+      var delta = currentDelta - previousTimeRef.current;
+
+      if (500 && delta < 1000 / 1) {
+        return;
+      }
+
+      const calculateCoordinates = () => {};
+
+      setCoords((prevCoords) =>
+        prevCoords.map(({ x, y }) => ({ x, y: y + 1 }))
+      );
+
+      previousTimeRef.current = currentDelta;
+    }
+    update();
+  }, []); // Make sure the effect runs only once
+
+  // React.useEffect(() => {
+  //   requestRef.current = requestAnimationFrame(animate);
+  //   return () => cancelAnimationFrame(requestRef.current);
+  // }, []);
+
+  // console.log(count);
+  console.log(coords[0].x, coords[0].y);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className={styles.root}>
+      {Array.from(Array(N).keys()).map((y) => {
+        return Array.from(Array(N).keys()).map((x) => {
+          return (
+            <div
+              key={`${x}-${y}`}
+              className={classNames(styles.gridItem, {
+                [styles.snake]: coords.find(
+                  (coord) => coord.x === x && coord.y === y
+                ),
+              })}
+            >{`${x}-${y}`}</div>
+          );
+        });
+      })}
+    </div>
+  );
 }
